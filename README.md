ea71550c64ee7b279c0f31a342b325a76fbabebf
tea9 <shaomiaojava@gmail.com>
2018-07-23 17:03:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## 挖新坑了！挖新坑了！
+[jetpack](https://developer.android.com/jetpack/docs/getting-started)了解一下  
+[分页库](https://developer.android.com/topic/libraries/architecture/paging)  
+[持久库](https://developer.android.com/reference/android/arch/persistence/room/Room)  
+
++ LiveData
++ ViewModel
++ NavigationController
---
+++ app/src/main/java/com/example/ui/start/StartFragment.kt
@@ -0,0 +1,30 @@
+package com.example.ui.start
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.R
+
+class StartFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = StartFragment()
+    }
+
+    private lateinit var viewModel: StartViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.start_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(StartViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
